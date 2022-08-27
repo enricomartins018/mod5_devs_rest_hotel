3fd87f42e5e4033b50a545929d04255c74e17052
Igor reis <igordiego237@gmail.com>
2022-08-27 15:38:56 -0300

    criação dos compentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header/Header";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" />
-      </Routes>
-    </Router>
-  );
+
+    <div>
+        OII
+    </div>
+    // <Router>
+    //   {/* <Navbar /> */}
+    //   <Routes>
+    //     <Route path="/" element={<Home/>} />
+    //   </Routes>
+    // </Router>
+  )
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import S  from './Header.module.css'
+
+export const Header = () => {
+  return (
+    <head >
+        <h1>Dev's Rest</h1>
+        <nav>
+            <a href="#">Sobre</a>
+            <a href="#">Contato</a>
+            <a href="#">Serviços</a>
+        </nav>
+    </head>
+  )
+}
