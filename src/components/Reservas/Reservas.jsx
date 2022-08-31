7955d9d2017e902932ff225b6b1bae3a1cf503f9
Rosana Ribeiro <rosana.fribeiro.90@gmail.com>
2022-08-31 20:39:04 -0300

    update de layoute da pagina de suas reservas

--- src/components/Reservas/Reservas.jsx
+++ src/components/Reservas/Reservas.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import S from "./Reservas.module.css";
 import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
+import foto from "../../assets/quarto.jpg";
 
 const Reservas = ({
   id,
@@ -12,22 +14,24 @@ const Reservas = ({
   dataEntrada,
   dataSaida,
 }) => {
+  const navigate = useNavigate();
+
   function deletar() {
     api.delete(`reservas/${id}`).then((response) => {
       console.log(response);
+      navigate("/");
     });
   }
 
   return (
-    <div className={S.container}>
-      <h1>Reservas</h1>
+    <div>
       <div className={S.text}>
         <h2 className={S.h2Reservas}>Id da reserva: {id}</h2>
         <h2 className={S.h2Reservas}>Seu Id: {id_Hospede}</h2>
         <h2 className={S.h2Reservas}>Número do seu quarto: {quarto}</h2>
-        <h2 className={S.h2Reservas}>Quantidade de camas: {quantLeitos}</h2>
-        <h2 className={S.h2Reservas}>Quantidade de adultos: {quantAdultos}</h2>
-        <h2 className={S.h2Reservas}>Quantidade de crianças: {quantCrian}</h2>
+        <h2 className={S.h2Reservas}>Quant. de camas: {quantLeitos}</h2>
+        <h2 className={S.h2Reservas}>Quant. de adultos: {quantAdultos}</h2>
+        <h2 className={S.h2Reservas}>Quant. de crianças: {quantCrian}</h2>
         <h2 className={S.h2Reservas}>Data de entrada: {dataEntrada}</h2>
         <h2 className={S.h2Reservas}>Data de saída: {dataSaida}</h2>
         <button className={S.btn}>Atualizar</button>
--- src/pages/SuasReservas/SuasReservas.jsx
+++ src/pages/SuasReservas/SuasReservas.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useNavigate } from "react";
 import Reservas from "../../components/Reservas/Reservas";
 import { api } from "../../services/api";
+import S from "./SuasReservas.module.css";
+import video from "../../assets/video.mp4";
+import foto from "../../assets/logo.svg";
 
 const SuasReservas = () => {
   if (!localStorage.getItem("id_Hospede")) {
@@ -48,17 +51,29 @@ const SuasReservas = () => {
       <section>
         {reserva.map((item, index) => {
           return (
-            <Reservas
-              id={item.id}
-              id_Hospede={item.id_Hospede}
-              quarto={item.quarto}
-              quantLeitos={item.quantLeitos}
-              quantAdultos={item.quantAdultos}
-              quantCrian={item.quantCrian}
-              dataEntrada={item.dataEntrada}
-              dataSaida={item.dataSaida}
-              key={index}
-            ></Reservas>
+            <div className={S.loginPage}>
+              <div className={S.overlay}></div>
+              <video src={video} autoPlay loop muted />
+              <div className={S.cardContainer}>
+                <div className={S.imageContainer}>
+                  <img className={S.imgFoto} src={foto}></img>
+                </div>
+                <div className={S.cardTitle}>Suas reservas</div>
+                <div className={S.cardBody}>
+                  <Reservas
+                    id={item.id}
+                    id_Hospede={item.id_Hospede}
+                    quarto={item.quarto}
+                    quantLeitos={item.quantLeitos}
+                    quantAdultos={item.quantAdultos}
+                    quantCrian={item.quantCrian}
+                    dataEntrada={item.dataEntrada}
+                    dataSaida={item.dataSaida}
+                    key={index}
+                  ></Reservas>
+                </div>
+              </div>
+            </div>
           );
         })}
       </section>
