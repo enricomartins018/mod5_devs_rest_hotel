b0ed58db8eb64036f35c722f3f3273d195d8db9e
enricomartins018 <enrico.martins018@gmail.com>
2022-09-01 16:31:22 -0300

    feat: Style importado.

--- src/pages/FacaSuaReserva/FacaSuaReserva.jsx
+++ src/pages/FacaSuaReserva/FacaSuaReserva.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import FormFacaSuaReserva from '../../components/FormFacaSuaReserva/FormFacaSuaReserva'
-
+import S from './FacaSuaReserva.module.css'
 
 const FacaSuaReserva = () => {
   return (
-    <div>
+    <div className={S.containerPage}>
       <FormFacaSuaReserva/>
     </div>
   );
