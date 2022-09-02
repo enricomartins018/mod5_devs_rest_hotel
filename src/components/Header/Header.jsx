cb73af45f53cd1dd635466d89c0f5a5cc07c834d
Igor reis <igordiego237@gmail.com>
2022-09-01 23:00:21 -0300

    ajuste nas classes do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,23 @@
-
 import React from "react";
-import { Button } from "../Button/Button";
+import Button from "../Button/Button";
 import S from "./Header.module.css";
 import logo from "../../assets/logo.svg";
 import { RiUser3Line } from "react-icons/ri";
 
 export const Header = () => {
   return (
-    <header>
+    <header className={S.header}>
       <img className={S.logo} src={logo} alt="" />
-      <nav>
-        <a href="#">Sobre nós</a>
-        <a href="#">Acomodações</a>
-        <a href="#">Serviços</a>
-        <a href="#">Reservar</a>
+      <nav className={S.nav}>
+        <a className={S.a} href="#">Sobre nós</a>
+        <a className={S.a} href="#">Acomodações</a>
+        <a className={S.a} href="#">Serviços</a>
+        <a className={S.a} href="#">Reservar</a>
       </nav>
-        <div className={S.btnReserva}>
-          <RiUser3Line className={S.icon} />
-          <Button className={S.Button} texto="Login" />
-        </div>
+      <div className={S.btnReserva}>
+        <RiUser3Line className={S.icon} />
+        <Button className={S.Button} texto="Login" />
+      </div>
     </header>
   );
 };
-
