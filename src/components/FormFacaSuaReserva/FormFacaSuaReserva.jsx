0e3e8478008e7c49402925dd0e17e758215e427d
enricomartins018 <enrico.martins018@gmail.com>
2022-09-03 19:53:55 -0300

    feat: Configurando containers de datas e de seleções.

--- src/components/FormFacaSuaReserva/FormFacaSuaReserva.jsx
+++ src/components/FormFacaSuaReserva/FormFacaSuaReserva.jsx
@@ -39,14 +39,18 @@ const FormFacaSuaReserva = () => {
   }
   return (
     <section className={S.section}>
-      <img className={S.logo} src={logo} />
+      <div className={S.voltar}>
+        <button className={S.btnVoltar}>Voltar</button>
+      </div>
       <form className={S.form}>
         <div className={S.selectDate}>
           <h2 className={S.titleForm}>
-            <b>Selecione suas datas</b>
+          <b>Selecione suas datas</b>
           </h2>
           <div className={S.dataEntrada}>
-            <label>Data de Entrada</label>
+            <label>
+              <b>ENTRADA</b>
+            </label>
             <input
               className={S.inputPattern}
               type="date"
@@ -58,7 +62,7 @@ const FormFacaSuaReserva = () => {
           </div>
           <div className={S.dataSaida}>
             <label>
-              <b>Data de Saída</b>
+              <b>SAÍDA</b>
             </label>
             <input
               className={S.inputPattern}
@@ -70,55 +74,57 @@ const FormFacaSuaReserva = () => {
             />
           </div>
         </div>
+        <img className={S.logo} src={logo} />
         <div className={S.containerSelectPattern}>
           <select
             className={S.selectPattern}
             value={dadosFormReserva.quarto}
             onChange={(e) => handleChange(e, "quarto")}
           >
-            <option>Selecione a quantidade de quartos:</option>
-            <option value="1">1 Quarto</option>
-            <option value="2">2 Quartos</option>
-            <option value="3">3 Quartos</option>
-            <option value="4">4 Quartos</option>
-            <option value="5">5 Quartos</option>
+            <option className={S.optionPattern} value="1">1 Quarto</option>
+            <option className={S.optionPattern} value="2">2 Quartos</option>
+            <option className={S.optionPattern} value="3">3 Quartos</option>
+            <option className={S.optionPattern} value="4">4 Quartos</option>
+            <option className={S.optionPattern} value="5">5 Quartos</option>
           </select>
           <select
             className={S.selectPattern}
             value={dadosFormReserva.quantLeitos}
             onChange={(e) => handleChange(e, "quantLeitos")}
           >
-            <option>Selecione a quantidade de leitos:</option>
-            <option value="1">1 Leito</option>
-            <option value="2">2 Leitos</option>
-            <option value="3">3 Leitos</option>
-            <option value="4">4 Leitos</option>
-            <option value="5">5 Leitos</option>
+            <option className={S.optionPattern} value="1">1 Leito</option>
+            <option className={S.optionPattern} value="2">2 Leitos</option>
+            <option className={S.optionPattern} value="3">3 Leitos</option>
+            <option className={S.optionPattern} value="4">4 Leitos</option>
+            <option className={S.optionPattern} value="5">5 Leitos</option>
           </select>
           <select
             className={S.selectPattern}
             value={dadosFormReserva.quantAdultos}
             onChange={(e) => handleChange(e, "quantAdultos")}
           >
-            <option>Selecione a quantidade de adultos:</option>
-            <option value="1">1 Adulto</option>
-            <option value="2">2 Adultos</option>
-            <option value="3">3 Adultos</option>
-            <option value="4">4 Adultos</option>
-            <option value="5">5 Adultos</option>
+            <option className={S.optionPattern} value="1">1 Adulto</option>
+            <option className={S.optionPattern} value="2">2 Adultos</option>
+            <option className={S.optionPattern} value="3">3 Adultos</option>
+            <option className={S.optionPattern} value="4">4 Adultos</option>
+            <option className={S.optionPattern} value="5">5 Adultos</option>
           </select>
           <select
             className={S.selectPattern}
             value={dadosFormReserva.quantCrian}
             onChange={(e) => handleChange(e, "quantCrian")}
           >
-            <option>Selecione a quantidade de crianças:</option>
-            <option value="1">1 Criança</option>
-            <option value="2">2 Crianças</option>
-            <option value="3">3 Crianças</option>
-            <option value="4">4 Crianças</option>
-            <option value="5">5 Crianças</option>
+            <option className={S.optionPattern} value="0">0 Criança</option>
+            <option className={S.optionPattern} value="1">1 Criança</option>
+            <option className={S.optionPattern} value="2">2 Crianças</option>
+            <option className={S.optionPattern} value="3">3 Crianças</option>
+            <option className={S.optionPattern} value="4">4 Crianças</option>
+            <option className={S.optionPattern} value="5">5 Crianças</option>
           </select>
+          <div className={S.containerCodigos}>
+            <input className={S.containerCodCorporativo} type="text" name="text" placeholder="Código Corporativo/Promo" />
+            <input className={S.containerCodGrupo} type="text" name="text" placeholder="Código De Grupo" />
+          </div>
           <div className={S.containerCheckBox}>
             <input className={S.inputCheckBox} type="checkbox" name="" id="" />
             <label className={S.labelCheckBox}>
