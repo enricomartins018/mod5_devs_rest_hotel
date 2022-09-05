e7c943e93915a8b7a11db96630e2095d0ce77ec6
Rosana Ribeiro <rosana.fribeiro.90@gmail.com>
2022-09-05 01:04:22 -0300

    correcao de link para reservas na header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,18 @@ import S from "./Header.module.css";
 import logo from "../../assets/logo.svg";
 import { RiUser3Line } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const navigate = useNavigate();
+
   function handlechange() {
-    navigate("/login");
+    if (!localStorage.getItem("id_Hospede")) {
+      const navigate = useNavigate();
+      navigate("/login");
+    }
   }
+
   return (
     <header className={S.header}>
       <img className={S.logo} src={logo} alt="" />
@@ -23,12 +29,13 @@ export const Header = () => {
         <a className={S.a} href="#">
           Serviços
         </a>
-        <a className={S.a} href="#">
+        <Link to="/facasuareserva" className={S.a} href="#">
           Reservar
-        </a>
+        </Link>
       </nav>
       <div className={S.btnReserva}>
         <RiUser3Line className={S.icon} />
+
         <Button className={S.Button} texto="Login" onClick={handlechange} />
       </div>
     </header>
--- src/pages/FacaSuaReserva/FacaSuaReserva.jsx
+++ src/pages/FacaSuaReserva/FacaSuaReserva.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import FormFacaSuaReserva from '../../components/FormFacaSuaReserva/FormFacaSuaReserva'
-import S from './FacaSuaReserva.module.css'
+import FormFacaSuaReserva from "../../components/FormFacaSuaReserva/FormFacaSuaReserva";
+import S from "./FacaSuaReserva.module.css";
+import { useNavigate } from "react-router-dom";
+
 const FacaSuaReserva = () => {
+  if (!localStorage.getItem("id_Hospede")) {
+    const navigate = useNavigate();
+    navigate("/login");
+  }
   return (
     <div className={S.containerPage}>
-      <FormFacaSuaReserva/>
+      <FormFacaSuaReserva />
     </div>
   );
 };
