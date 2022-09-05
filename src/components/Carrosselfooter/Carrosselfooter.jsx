955bb132d9f7d581299ac726e2623657f576514c
Rosana Ribeiro <rosana.fribeiro.90@gmail.com>
2022-09-05 00:29:06 -0300

    inserindo fonte padrao do site e correcao de fonte do footer

--- src/components/Carrosselfooter/Carrosselfooter.jsx
+++ src/components/Carrosselfooter/Carrosselfooter.jsx
@@ -1,30 +1,26 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel'
+import React from "react";
+import Carousel from "react-material-ui-carousel";
 import S from "./Carrossel.module.css";
 
 export const Carrossel = (props) => {
-    var items = [
-        {name: "Você merece um descanso!",},
-        {name: "Colecione bons momentos com a gente!",},
-        {name: "Às vezes um descanso é a melhor forma de ser produtivo!",},
-        {name: "Depois que você vir, difícil vai ser não querer voltar!",},
-        {name: "Keep Calm e venha se hospedar!",},
-        {name: "Dev´s Rest Hotel",}
-    ]
+  var items = [
+    { name: "Você merece um descanso!" },
+    { name: "Colecione bons momentos com a gente!" },
+    { name: "Às vezes um descanso é a melhor forma de ser produtivo!" },
+    { name: "Depois que você vir, difícil vai ser não querer voltar!" },
+    { name: "Keep Calm e venha se hospedar!" },
+    { name: "Dev´s Rest Hotel" },
+  ];
 
-    return (
-        <Carousel className={S.carrossel}>
-            {
-                items.map( (item, i) => <Item key={i} item={item} /> )
-            }
-        </Carousel>
-    )
-}
+  return (
+    <Carousel className={S.carrossel}>
+      {items.map((item, i) => (
+        <Item key={i} item={item} />
+      ))}
+    </Carousel>
+  );
+};
 
-function Item(props)
-{
-    return (
-        <p><b>{props.item.name}</b></p>
-        
-    )
+function Item(props) {
+  return <p>{props.item.name}</p>;
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import { CarouselServicos } from '../SectionCarouselServicos/CarouselServicos'
-import { Caroussel } from '../SectionCaroussel/Caroussel'
-import SectionServicos from '../SectionServicos/SectionServicos'
+import React from "react";
+import { CarouselServicos } from "../SectionCarouselServicos/CarouselServicos";
+import { Caroussel } from "../SectionCaroussel/Caroussel";
+import SectionServicos from "../SectionServicos/SectionServicos";
 // import { CarouselServicos } from '../SectionSobre/Carousel/CarouselServicos'
-import  {SectionSobre}  from '../SectionSobre/SectionSobre'
-import S from './Main.module.css'
+import { SectionSobre } from "../SectionSobre/SectionSobre";
+import S from "./Main.module.css";
 
 export const Main = () => {
   return (
     <main className={S.main}>
-      
-        <SectionSobre />
-        <Caroussel />
-        <SectionServicos />
+      <SectionSobre />
+      <Caroussel />
+      <SectionServicos />
       <CarouselServicos />
-       
     </main>
-  )
-}
+  );
+};
